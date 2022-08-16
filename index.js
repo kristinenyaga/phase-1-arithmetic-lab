ac733c5d5d98dea99c2aedfd1686c84b5c8da570
krisine <kristine.nyaga@student.moringaschool.com>
2022-08-16 14:12:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1=31
+let num2=2
+let multiply=num1*num2
 // Write your code here
+let random=Math.floor(Math.random()*10)+1
+let num4=11
+let num3=7
+let mod=num4%num3
+let max=Math.max(20,1,4,3,2)
